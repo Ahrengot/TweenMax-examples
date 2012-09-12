2c4747cbec66ead0a3b5e5941b8a87913d68bbd4
Jens Ahrengot Boddum <ahrengot@gmail.com>
2012-09-12 20:59:50 +0200

    Added simple tween example

--- simple/index.php
+++ simple/index.php
@@ -31,23 +31,44 @@
   
   	<link rel="stylesheet" href="styles/css/reset.css">
   	<style type="text/css">
-  		#container > header, #main { width: 80%; margin: auto; }
+  		#container > header, #main { width: 80%; margin: 100px auto; }
+
+  		#box {
+			position: absolute;
+			width: 50px; height: 50px;
+			top: 50%; left: 50%;
+			margin: -25px 0 0 -25px;
+			background: #bada55;
+			border-radius: 3px;
+			-webkit-backface-visibility:hidden;
+		}
+
+		html { cursor: pointer; }
   	</style>
 </head>
 
 <body>
 
 	<div id="container">
-		<header>
-			<h1><?= $title; ?></h1>
-		</header>
+		<footer id="site-header" class="clearfix">
+			<section class="inner-footer">
+				<a href="http://ahrengot.com/" title="Go to my Website (Opens in a new window)" target="_blank"><img src="http://ahrengot.com/wp-content/themes/ahrengot-dev/library/img/ahrengot-logo.png" width="204" height="59" alt="Jens Ahrengot Boddum" style="margin-left: -10px;" /></a>
+				<a id="back-to-tutorial" class="right" href="<?= $tutorial_url; ?>" target="_self">Back to tutorial</a>
+			</section>
+		</footer>
 		<div id="main" role="main">
-			
+			<header>
+				<h1><?= $title; ?></h1>
+				<p>Click anywhere to animate the box</p>
+			</header>
+
+			<section id="canvas">
+				<div id="box"></div>
+			</section>
 		</div>
 		<footer id="site-footer" class="clearfix">
 			<section class="inner-footer">
-				<a id="back-to-tutorial" href="<?= $tutorial_url; ?>" target="_self">Back to tutorial</a>
-				<a href="http://ahrengot.com/" title="Go to my Website (Opens in a new window)" target="_blank"><img class="right" src="http://ahrengot.com/wp-content/themes/ahrengot-dev/library/img/ahrengot-logo.png" width="204" height="59" alt="Jens Ahrengot Boddum" /></a>
+				<p>My name is Jens Ahrengot Boddum and I am a freelance designer and developer. <a href="http://ahrengot.com/work" target="_blank">Here's my portfolio.</a></p>
 			</section>
 		</footer>
 	</div> <!-- eo #container -->
@@ -58,9 +79,20 @@
   	<script src="js/libs/gs/TweenMax.min.js"></script>
 
 	<script type="text/javascript">
-		$(function() {
-			log('DOM Ready ...');
-		});
+		var $box = $('#box');
+
+		function moveBox(e) {
+			console.log('Click!');
+			var x = e.pageX,
+				y = e.pageY;
+
+			TweenMax.to($box, 1.4, {
+				css: { left: x, top: y, scale: Math.random() * 2 + 1 },
+				ease:Elastic.easeOut
+			});
+		}
+
+		$(window).on('click', moveBox);
 	</script>
 </body>
 </html>
